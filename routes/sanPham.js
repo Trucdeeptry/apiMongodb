af81420248856f36b7de5122cec277edb3cea706
Trucdeeptry <phantruc438@gmail.com>
2024-11-10 15:42:19 +0700

    id

--- routes/sanPham.js
+++ routes/sanPham.js
@@ -9,11 +9,11 @@ module.exports = (client) => {
     // Add a new product
     router.post("/", (req, res) => sanphamController.addSanPham(req, res, client));
 
-    // Update a product by ma_sp
-    router.put("/:ma_sp", (req, res) => sanphamController.updateSanPham(req, res, client));
+    // Update a product by _id
+    router.put("/:_id", (req, res) => sanphamController.updateSanPham(req, res, client));
 
-    // Delete a product by ma_sp
-    router.delete("/:ma_sp", (req, res) => sanphamController.deleteSanPham(req, res, client));
+    // Delete a product by _id
+    router.delete("/:_id", (req, res) => sanphamController.deleteSanPham(req, res, client));
 
     return router;
 };
