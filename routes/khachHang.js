cc1e9b13e62149b6a945ff0c3fb3ef5238648a31
Trucdeeptry <phantruc438@gmail.com>
2024-11-11 23:30:10 +0700

    fix kh

--- routes/khachHang.js
+++ routes/khachHang.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 module.exports = (client) => {
     router.get("/", (req, res) => khachHangController.getAllKhachHang(req, res, client));
-    router.post("/", (req, res) => khachHangController.addNhaKhachHang(req, res, client));
+    router.post("/", (req, res) => khachHangController.addKhachHang(req, res, client));
 
     // Update a product by ma_sp
-    router.put("/:_id", (req, res) => khachHangController.updateNhaKhachHang(req, res, client));
+    router.put("/:_id", (req, res) => khachHangController.updateKhachHang(req, res, client));
 
     // Delete a product by ma_sp
-    router.delete("/:_id", (req, res) => khachHangController.deleteNhaKhachHang(req, res, client));
+    router.delete("/:_id", (req, res) => khachHangController.deleteKhachHang(req, res, client));
     return router;
 };
 
